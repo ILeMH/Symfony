6dd7e7d8dbd303b44f78e46d8df26046d0a4bbfa
Alexandre SCHANNE <alexandre.schanne@telecomanancy.eu>
2016-12-05 09:44:43 +0100

    premier commit

---
+++ src/OC/CoreBundle/Controller/PrincipalController.php
@@ -0,0 +1,38 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace OC\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class PrincipalController extends Controller
+{
+
+    public function homeAction()
+    {
+        $listAdverts = array(
+            array(
+            'title'   => 'Recherche développpeur Symfony',
+            'id'      => 1,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()),
+          array(
+            'title'   => 'Mission de webmaster',
+            'id'      => 2,
+            'author'  => 'Hugo',
+            'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+            'date'    => new \Datetime()),
+          array(
+            'title'   => 'Offre de stage webdesigner',
+            'id'      => 3,
+            'author'  => 'Mathieu',
+            'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+            'date'    => new \Datetime())
+    );
+        return $this->render('OCCoreBundle:vueP:index.html.twig', array('listAdverts' => $listAdverts));
+    }
+}
